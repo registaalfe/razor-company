61aee038673f4d33ef4f635be239c317188c14f5
Alfe Christadeus <69341857+registaalfe@users.noreply.github.com>
2024-01-11 07:00:47 +0700

    Add Card On About Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        cards: ["0 4px 18px 0 rgb(0 0 0 / 0.1)"],
+      },
+    },
   },
   plugins: [],
 };
